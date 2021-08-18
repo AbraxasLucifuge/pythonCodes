2fad862e37446cb1ac400e5db5f67663984c5dff
chanakyakapoor <36603818+chanakyakapoor@users.noreply.github.com>
2021-08-18 12:10:45 +0530

    Add files via upload

---
+++ insert_bst.cpp
@@ -0,0 +1,71 @@
+#include <stdio.h>
+#include <stdlib.h>
+struct Node
+{
+    struct Node *lchild;
+    int data;
+    struct Node *rchild;
+} *root = NULL;
+struct Node *RInsert(struct Node *p, int key)
+{
+    struct Node *t = NULL;
+
+    if (p == NULL)
+    {
+        t = (struct Node *)malloc(sizeof(struct Node));
+        t->data = key;
+        t->lchild = t->rchild = NULL;
+        return t;
+    }
+    if (key < p->data)
+        p->lchild = RInsert(p->lchild, key);
+    else if (key > p->data)
+        p->rchild = RInsert(p->rchild, key);
+
+    return p;
+}
+void Inorder(struct Node *p)
+{
+    if (p)
+    {
+        Inorder(p->lchild);
+        printf("%d ", p->data);
+        Inorder(p->rchild);
+    }
+}
+struct Node *Search(int key)
+{
+    struct Node *t = root;
+
+    while (t != NULL)
+    {
+        if (key == t->data)
+            return t;
+        else if (key < t->data)
+            t = t->lchild;
+        else
+            t = t->rchild;
+    }
+    return NULL;
+}
+int main()
+{
+    struct Node *temp;
+
+    root = RInsert(root, 50);
+    RInsert(root, 10);
+    RInsert(root, 40);
+    RInsert(root, 20);
+    RInsert(root, 30);
+
+    Inorder(root);
+    printf("\n");
+
+    temp = Search(20);
+    if (temp != NULL)
+        printf("element %d is found\n", temp->data);
+    else
+        printf("element is not found\n");
+
+    return 0;
+}
